3f1b523ff0719f84771427260e0f1ebf61e5fb95
Brad S <brad@adrenalizedigital.ca>
2023-07-13 11:46:09 -0600

    core web vitals optimization

--- app/(pages)/page.jsx
+++ app/(pages)/page.jsx
@@ -1,12 +1,9 @@
 import Hero from "../components/Hero"
 
-import { agency } from "@/data"
-import Link from "next/link"
-import Image from "next/image"
-
 import { useBreadcrumbJSON } from "../hooks"
 import dynamic from "next/dynamic"
 
+const LandingContent = dynamic(() => import('@app/components/LandingContent'))
 const Contact = dynamic(() => import('@/app/components/Contact'))
 const Featured = dynamic(() => import('../components/Featured'))
 const Regions = dynamic(() => import('../components/Regions'))
@@ -25,27 +22,7 @@ export default async function Home() {
     <>
       <JsonLd data={breadcrumbData} />
       <Hero />
-      <div className="flex flex-col lg:flex-row space-y-16 lg:space-y-0 lg:space-x-8 px-4 py-8 md:px-8 lg:py-16">
-        <div className="flex flex-col space-y-8 lg:space-y-auto lg:w-1/2">
-          <h2 className="text-3xl md:text-4xl font-bold pb-2 border-b">
-            Vallarta Real Estate Experts
-          </h2>
-          <p className="lg:mb-auto lg:grow">
-            {agency.description}
-          </p>
-          <Link href="/contact" className="button lg:mt-auto">
-            Contact PV Coastal Realty Today!
-          </Link>
-        </div>
-        <div className="relative hidden lg:block lg:w-1/2 h-auto min-h-[350px]">
-          <Image 
-            src="/beach-family.webp"
-            fill={true}
-            className="rounded-r-md rounded-l-sm object-cover"
-            alt="Contact PV Coastal Realty today and let us guide you in turning your property dreams into reality!"
-          />
-        </div>
-      </div>
+      <LandingContent />
       <Featured />
       <Regions />
       <Contact />
---
+++ app/components/LandingContent.jsx
@@ -0,0 +1,31 @@
+import { agency } from "@/data"
+import Link from "next/link"
+import Image from "next/image"
+
+const LandingContent = () => {
+  return (
+<div className="flex flex-col lg:flex-row space-y-16 lg:space-y-0 lg:space-x-8 px-4 py-8 md:px-8 lg:py-16">
+        <div className="flex flex-col space-y-8 lg:space-y-auto lg:w-1/2">
+          <h2 className="text-3xl md:text-4xl font-bold pb-2 border-b">
+            Vallarta Real Estate Experts
+          </h2>
+          <p className="lg:mb-auto lg:grow">
+            {agency.description}
+          </p>
+          <Link href="/contact" className="button lg:mt-auto">
+            Contact PV Coastal Realty Today!
+          </Link>
+        </div>
+        <div className="relative hidden lg:block lg:w-1/2 h-auto min-h-[350px]">
+          <Image 
+            src="/beach-family.webp"
+            fill={true}
+            className="rounded-r-md rounded-l-sm object-cover"
+            alt="Contact PV Coastal Realty today and let us guide you in turning your property dreams into reality!"
+          />
+        </div>
+      </div>
+  )
+}
+
+export default LandingContent
