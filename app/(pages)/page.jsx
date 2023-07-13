5d87e06cec8c467700ba52ea8196e763a12a4d9c
Brad S <brad@adrenalizedigital.ca>
2023-07-13 11:52:26 -0600

    update import path for landing content

--- app/(pages)/page.jsx
+++ app/(pages)/page.jsx
@@ -3,11 +3,11 @@ import Hero from "../components/Hero"
 import { useBreadcrumbJSON } from "../hooks"
 import dynamic from "next/dynamic"
 
-const LandingContent = dynamic(() => import('@app/components/LandingContent'))
+const LandingContent = dynamic(() => import('@/app/components/LandingContent'))
 const Contact = dynamic(() => import('@/app/components/Contact'))
-const Featured = dynamic(() => import('../components/Featured'))
-const Regions = dynamic(() => import('../components/Regions'))
-const JsonLd = dynamic(() => import('../components/JsonLd'))
+const Featured = dynamic(() => import('@/app/components/Featured'))
+const Regions = dynamic(() => import('@/app/components/Regions'))
+const JsonLd = dynamic(() => import('@/app/components/JsonLd'))
 
 export default async function Home() {
 
