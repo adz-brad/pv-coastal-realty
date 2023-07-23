ff9ef34fb4adb8c2d965b055324c9688b9fb63a7
Brad S <brad@adztech.io>
2023-07-23 14:05:10 -0600

    update config

--- next.config.js
+++ next.config.js
@@ -1,29 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    async headers() {
-        return [
-          {
-            source: '/(.*).jpg',
-            headers: [
-              {
-                key: 'Cache-Control',
-                value:
-                  'public, max-age=2419200, s-maxage=2419200, stale-while-revalidate=2419200',
-              },
-            ],
-          },
-          {
-            source: '/_next/image(.*)',
-            headers: [
-              {
-                key: 'Cache-Control',
-                value: 'public, max-age=2419200, s-maxage=2419200, stale-while-revalidate=2419200',
-              },
-            ],
-          },
-        ]
-      },
     compiler: {
         removeConsole: process.env.NODE_ENV === "production"
       },
