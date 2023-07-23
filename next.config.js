d25d646f2b85f9d9b9573bb08f1945e4d8d1ffc2
Brad S <brad@adztech.io>
2023-07-23 12:58:05 -0600

    remove console logging

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+    compiler: {
+        removeConsole: process.env.NODE_ENV === "production"
+      },
     images: {
         formats: ['image/avif', 'image/webp'],
         unoptimized: true,
