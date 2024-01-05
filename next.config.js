cfb7381cd41984e007099f1f5de769d144bc5b35
Brad S <brad@adztech.io>
2024-01-05 11:51:25 -0600

    remove unoptimzed

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
         removeConsole: process.env.NODE_ENV === "production"
       },
     images: {
-        unoptimized: process.env.NODE_ENV === "development",
+        //unoptimized: process.env.NODE_ENV === "development",
         remotePatterns: [
             { hostname: 'cdn.sanity.io' },
             { hostname: 'images.unsplash.com' },
